a659e757c73fc3e15b571616dd81ad4597121c4a
seongjin-an <seongjin4us@gmail.com>
2022-03-08 01:25:20 +0900

    http client

--- myclient/src/main/kotlin/Main.kt
+++ myclient/src/main/kotlin/Main.kt
@@ -1,8 +1,42 @@
 import org.apache.hc.client5.http.classic.methods.HttpGet
+import org.apache.hc.core5.http.NameValuePair
+import org.apache.hc.core5.http.message.BasicNameValuePair
 import org.apache.hc.core5.net.URIBuilder
 
 fun main(){
-    val httpGet = HttpGet("https://example.com/imsi")
-    val uri = URIBuilder(httpGet.uri).addParameter("ansj", "111").build()
+    val httpGet = HttpGet("https://example.com")
+    println(httpGet.uri.host)
+    println(httpGet.requestUri)
+    println(httpGet.path)
+    val uri = URIBuilder("https://example.com").addParameter("ansj", "111").build()
+    val uri2 = URIBuilder("https://example.com")
+        .addParameters(listOf<NameValuePair>(BasicNameValuePair("ansj", "123"), BasicNameValuePair("ansj", "33"))).build()
     println("uri: $uri")
+    println("uri2: $uri2")
+
+    println()
+    println()
+    val list = listOf("ansj" to "123", "ansj2" to "456")
+    val uri3 = URIBuilder("https://ansj.com")
+    list.forEach{
+        uri3.addParameter(it.first, it.second)
+    }
+    uri3.build()
+    println("uri3: $uri3")
+    val parameter = listOf<Pair<String, Any>>("ansj" to 1111, "ansj2" to "2222")
+    val url = URIBuilder("https://ansj.com")
+    parameter.forEach{
+        url.addParameter(it.first, it.second.toString())
+    }
+    url.build()
+    println("url: $url")
+
+    val map = mapOf("ansj" to "123", "ansj2" to 222)
+
+    map.entries.forEach{
+        println("key: ${it.key} / value: ${it.value}")
+    }
+
+    val result = httpGet("https://jsonplaceholder.typicode.com/posts")
+    println("result: $result")
 }
--- myclient/src/main/kotlin/httpHelper.kt
+++ myclient/src/main/kotlin/httpHelper.kt
@@ -1,15 +1,37 @@
 import javafx.util.Pair
 import org.apache.hc.client5.http.classic.methods.HttpGet
+import org.apache.hc.client5.http.config.RequestConfig
 import org.apache.hc.client5.http.impl.classic.HttpClients
+import org.apache.hc.core5.http.HttpEntity
+import org.apache.hc.core5.http.io.entity.EntityUtils
+import org.apache.hc.core5.net.URIBuilder
+import org.apache.hc.core5.util.Timeout
+import java.io.IOException
+import java.net.URISyntaxException
 
 fun httpGet(
-    url: String,
+    reqUrl: String,
     parameter: List<Pair<String, Any>>? = null,
     header: Map<String, Any>? = null,
-    timeout: Int = 3000
-){
+    timeout: Long = 3000
+): String?{
+    var result: String? = null
     val httpClient = HttpClients.createDefault()
-
-    val httpGet = HttpGet(url)
-//    httpGet.
+    val url = URIBuilder(reqUrl)
+    parameter?.forEach{ url.addParameter(it.key, it.value.toString()) }
+    try {
+        val httpGet = HttpGet(url.build())
+        httpGet.apply{ config = RequestConfig.custom().setConnectTimeout(Timeout.ofMilliseconds(timeout)).build() }
+        httpGet.setHeader("Content-Type", "application/json")
+        header?.entries?.forEach { httpGet.setHeader(it.key, it.value) }
+        val response = httpClient.execute(httpGet)
+        val entity: HttpEntity = response.entity
+        result = EntityUtils.toString(entity)
+        EntityUtils.consume(entity)
+    }catch(error: URISyntaxException){
+        error.printStackTrace()
+    }catch(error: IOException){
+        error.printStackTrace()
+    }
+    return result
 }
