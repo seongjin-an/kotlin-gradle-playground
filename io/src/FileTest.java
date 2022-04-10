743314cac69c4c3b9f4d8cfd719f400161dee25d
seongjin-an <seongjin4us@gmail.com>
2022-04-10 19:50:27 +0900

    file, randomAccessFile

---
+++ io/src/FileTest.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+
+public class FileTest {
+    public static void main(String[] args) throws IOException {
+        File file = new File("/home/seongjin/workspace/_/gradle_kotlin_playground/input2.txt");
+        file.createNewFile();
+        System.out.println(file.isFile());
+        System.out.println(file.isDirectory());
+        System.out.println(file.getName());
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getPath());
+        System.out.println(file.canRead());
+        System.out.println(file.canWrite());
+        file.delete();
+    }
+}
