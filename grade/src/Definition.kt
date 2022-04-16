6f11be66a6628dbcda037d13dbfacb4c2979c5f7
seongjin-an <seongjin4us@gmail.com>
2022-04-16 14:27:34 +0900

    add grade

---
+++ grade/src/Definition.kt
@@ -0,0 +1,8 @@
+object Definition {
+    const val KOREAN = 1001
+    const val MATH = 2001
+
+    const val AB_TYPE = 0
+    const val SAB_TYPE = 1
+    const val PF_TYPE = 2
+}
---
+++ grade/src/Subject.kt
@@ -0,0 +1,10 @@
+class Subject(var subjectName: String, var subjectId: Int) {
+    var gradeType: Int = 0
+
+    init {
+        this.gradeType = Definition.AB_TYPE
+    }
+
+
+
+}
