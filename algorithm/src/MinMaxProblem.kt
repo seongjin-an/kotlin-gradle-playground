bb62be84e6efebbe2adaf0d96a303c33d3a01057
seongjin-an <seongjin4us@gmail.com>
2022-04-21 23:46:01 +0900

    algorithm

---
+++ algorithm/src/MinMaxProblem.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val nums: Array<Int> = arrayOf(10, 55, 23, 2, 79, 101, 16, 82, 30, 45)
+    var min = 0
+    var max = 0
+    for(i in 0 until nums.size){
+        if( nums[i] < min ){
+            min = nums[i]
+        }
+        if( nums[i] > max ) {
+            max = nums[i]
+        }
+    }
+    println("min: $min")
+    println("max: $max")
+}
