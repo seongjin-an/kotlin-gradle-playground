cdc9973431bcb42908bced73cb7246037171422a
seongjin-an <seongjin4us@gmail.com>
2022-04-23 23:55:10 +0900

    algorithm

--- algorithm/src/MinMaxProblem.kt
+++ algorithm/src/MinMaxProblem.kt
@@ -2,14 +2,18 @@ fun main() {
     val nums: Array<Int> = arrayOf(10, 55, 23, 2, 79, 101, 16, 82, 30, 45)
     var min = nums[0]
     var max = nums[0]
+    var minPos = 0;
+    var maxPos = 0;
     for(i in 0 until nums.size){
         if( nums[i] < min ){
             min = nums[i]
+            minPos = i + 1;
         }
         if( nums[i] > max ) {
             max = nums[i]
+            maxPos = i + 1;
         }
     }
-    println("min: $min")
-    println("max: $max")
+    println("min: $min, min index: $minPos")
+    println("max: $max, max index: $maxPos")
 }
